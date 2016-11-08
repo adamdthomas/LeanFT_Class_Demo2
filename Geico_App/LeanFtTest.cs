e323ca92fd99d10db5dd31b77608e76e9db95675
Adam THomas <adamdthomas@gmail.com>
2016-11-08 08:44:00 -0500

    Initial Commit

---
+++ Geico_App/LeanFtTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+using HP.LFT.SDK.Web;
+
+namespace Geico_App
+{
+    [TestFixture]
+    public class LeanFtTest : UnitTestClassBase
+    {
+        IBrowser browser;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            // Setup once per fixture 
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Before each test
+            browser = BrowserFactory.Launch(BrowserType.InternetExplorer);
+            browser.Navigate("https://auto-buy-gz-user1.geico.com/");
+
+
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up after each test
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            // Clean up once per fixture
+        }
+    }
+}
